ee94659dad442e461a803acaa7291a040272aba7
WuYi <362223054@qq.com>
2021-08-19 21:23:06 +0800

    到测试

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         int a = 2;
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
 
     }
 }
