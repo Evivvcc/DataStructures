9454fbc64dc2f2f713c72e64a5587a20a6b58676
Wuyi <89005477+Evivvcc@users.noreply.github.com>
2021-08-20 00:06:09 +0800

    Delete src directory

--- src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        int a = 2;
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-
-    }
-}
